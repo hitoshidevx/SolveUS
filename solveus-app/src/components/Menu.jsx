a361d17feed3a904eba7ae40b18bc51aa0a64eaa
hitoshidevx <g.hitoshi.yokogawa@gmail.com>
2022-10-08 09:38:27 -0300

    Iniciando projeto

---
+++ solveus-app/src/components/Menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const menuStyle = {
+    backgroundColor: '#727DBA',
+    color: 'white'
+}
+
+function Menu() {
+  return(
+    <div style={menuStyle}>
+        <h1>SolveUS</h1>
+    </div>
+  )
+}
+
+export default Menu;
